c84f731d74912abcfbf4a383b34e8906c24d96db
mrafing <dwpu110300@gmail.com>
2024-06-16 20:16:11 +0700

    first commit

---
+++ constant.php
@@ -0,0 +1,30 @@
+<?php
+// define('NAMA', "Rafi");
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 24;
+// echo UMUR;
+
+
+// class Coba {
+//     const NAMA = "Rafi";
+// }
+
+// echo Coba::NAMA;
+
+// echo __DIR__;
+
+// function coba () {
+//     return __FUNCTION__;
+// }
+
+// echo coba();
+
+// class Coba {
+//     public $kelas = __CLASS__;
+// }
+
+// $obj = new Coba();
+// echo $obj->kelas;
